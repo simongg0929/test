c2cf5047d8b1270c6643415b7fdb4a8a66c725c9
simongg0929 <simongg0929@gmail.com>
2025-10-23 12:49:19 +0800

    one

--- 6-6-4.php
+++ 6-6-4.php
@@ -1,8 +1,8 @@
 <?php
 $arr = array(
     "sid" =>"C112181112",
-    "name" =>"smith leo",
-    "shape" =>"triangle",
+    "name" =>"傅瀚鋌",
+    "shape" =>"三角形",
     100
 );
 foreach($arr as $key => $value) echo "$key => $value <BR>";
